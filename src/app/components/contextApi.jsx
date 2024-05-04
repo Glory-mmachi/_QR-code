4929808834154949bb62d9c19f4766a1414de3b4
Glory-mmachi <105730131+Glory-mmachi@users.noreply.github.com>
2024-05-04 22:27:07 +0100

    removed backgroundImage

--- src/app/components/contextApi.jsx
+++ src/app/components/contextApi.jsx
@@ -4,7 +4,7 @@ import { createContext, useContext, useState } from "react";
 const ThemeContext = createContext();
 
 export default function ThemeProvider({ children }) {
-  const [val, setVal] = useState("Jude");
+  const [val, setVal] = useState("Glory");
 
   function handleChange(e) {
     setText(e.target.value);
